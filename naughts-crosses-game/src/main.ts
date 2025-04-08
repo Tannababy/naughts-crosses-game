9d55e884f8d6e17abfab19477657b381df9ea3f2
Lotanna <tannaloamobi@gmail.com>
2025-04-08 16:04:09 +0100

    edited readMe with project breakdown

--- naughts-crosses-game/src/main.ts
+++ naughts-crosses-game/src/main.ts
@@ -1,10 +1,10 @@
-import "./style.css";
+import "./style.scss";
 
 //Selecting HTML elements through the DOM
 
 const boxContainer = document.querySelector<HTMLElement>(".box");
 console.log(boxContainer);
 const squareBtn =
-  document.querySelectorAll<HTMLButtonElement>("box__row--btn");
+  document.querySelectorAll<HTMLButtonElement>(".box__row-btn");
 
 console.log(squareBtn);
