d76000128c2b663c06585cf138a399e9a9cec43b
Lotanna <tannaloamobi@gmail.com>
2025-04-09 21:21:43 +0100

    created ts logic to handle event listeners and display current player

--- naughts-crosses-game/src/main.ts
+++ naughts-crosses-game/src/main.ts
@@ -1,10 +1,19 @@
 import "./style.scss";
 
 //Selecting HTML elements through the DOM
+const tiles = document.querySelectorAll<HTMLDivElement>(".box__row-tile");
 
-const boxContainer = document.querySelector<HTMLElement>(".box");
-console.log(boxContainer);
-const squareBtn =
-  document.querySelectorAll<HTMLButtonElement>(".box__row-btn");
+let currentPlayer: string = "X";
 
-console.log(squareBtn);
+const handleClick = (event: Event) => {
+  const target = event.currentTarget as HTMLDivElement;
+
+  // to check if tile is filled
+  if (target.innerText !== "") {
+    return;
+  }
+
+  target.innerText = currentPlayer;
+};
+
+tiles.forEach((tile) => tile.addEventListener("click", handleClick));
